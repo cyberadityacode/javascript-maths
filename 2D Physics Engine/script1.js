97f83e0d83724f03592aa89042f1e089aba20a6f
cyberadityacode <adityadubey793@gmail.com>
2026-03-29 10:51:40 +0530

    draw a circle

--- 2D Physics Engine/script1.js
+++ 2D Physics Engine/script1.js
@@ -1,1 +1,11 @@
 console.log("2D physics Engine");
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.arc(100,100,20,0,2 * Math.PI);
+ctx.strokeStyle = "black";
+ctx.stroke();
+ctx.fillStyle = "red";
+ctx.fill();
