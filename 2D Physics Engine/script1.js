b7b983b5e1986f676e718eaf637c6eed381c340b
cyberadityacode <adityadubey793@gmail.com>
2026-03-29 10:55:58 +0530

    created a function to draw a ball

--- 2D Physics Engine/script1.js
+++ 2D Physics Engine/script1.js
@@ -3,9 +3,22 @@ console.log("2D physics Engine");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-ctx.beginPath();
+/* ctx.beginPath();
 ctx.arc(100,100,20,0,2 * Math.PI);
 ctx.strokeStyle = "black";
 ctx.stroke();
 ctx.fillStyle = "red";
-ctx.fill();
+ctx.fill(); */
+
+
+function drawBall(x,y,r){
+    ctx.beginPath();
+    ctx.arc(x,y,r,0, 2 * Math.PI);
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+    ctx.fillStyle = "red";
+    ctx.fill();
+}
+
+drawBall(100,100,20);
+drawBall(200,200,30);
