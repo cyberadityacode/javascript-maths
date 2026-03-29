027de7d47ec2efa238578563453b3f2a3fc56ac1
cyberadityacode <adityadubey793@gmail.com>
2026-03-29 11:30:58 +0530

    add event listener to track key movements

--- 2D Physics Engine/lesson2-move a ball/script2.js
+++ 2D Physics Engine/lesson2-move a ball/script2.js
@@ -12,5 +12,21 @@ function drawBall(x,y,radius){
     ctx.fill();
 }
 
+canvas.addEventListener("keydown", function(e){
+    // console.log(e);
+    if(e.key === "ArrowLeft"){
+        console.log("arrow left pressed");
+    }
+    if(e.key === "ArrowRight"){
+        console.log("arrow right pressed");
+    }
+    if(e.key === "ArrowUp"){
+        console.log("arrow up pressed");
+    }
+    if(e.key === "ArrowDown"){
+        console.log("arrow down pressed");
+    }
+});
+
 drawBall(100,100,20);
 drawBall(200,200,30);
