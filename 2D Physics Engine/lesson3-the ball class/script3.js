3a6339f4a7ce7fea3d2e1f47a1615fb960733d08
cyberadityacode <adityadubey793@gmail.com>
2026-03-30 22:04:48 +0530

    optimized code

--- 2D Physics Engine/lesson3-the ball class/script3.js
+++ 2D Physics Engine/lesson3-the ball class/script3.js
@@ -3,7 +3,7 @@ console.log("lesson 3");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-let LEFT,RIGHT,UP,DOWN;
+let LEFT = false, RIGHT = false, UP = false, DOWN = false;
 
 class Ball{
     constructor(x,y,r){
@@ -20,81 +20,42 @@ class Ball{
         ctx.fillStyle = "red";
         ctx.fill(); 
     }
-
-
 }
 
-
-
-canvas.addEventListener("keydown", function(e){
-    if(e.key === "ArrowRight"){
-      console.log("right");
-      RIGHT = true;
-    }
-    if(e.key==="ArrowLeft"){
-        console.log("left");
-        LEFT = true;
-    }
-    if(e.key === "ArrowUp"){
-        console.log("up");
-        UP = true;
-    }
-    if(e.key === "ArrowDown"){
-        console.log("down");
-        DOWN = true;
-    }
+document.addEventListener("keydown", function(e){
+    if(e.key === "ArrowRight") RIGHT = true;
+    if(e.key === "ArrowLeft") LEFT = true;
+    if(e.key === "ArrowUp") UP = true;
+    if(e.key === "ArrowDown") DOWN = true;
 });
 
-canvas.addEventListener("keyup", function(e){
-     if(e.key === "ArrowRight"){
-      console.log("right");
-      RIGHT = false;
-    }
-    if(e.key==="ArrowLeft"){
-        console.log("left");
-        LEFT = false;
-    }
-    if(e.key === "ArrowUp"){
-        console.log("up");
-        UP = false;
-    }
-    if(e.key === "ArrowDown"){
-        console.log("down");
-        DOWN = false;
-    }
+document.addEventListener("keyup", function(e){
+    if(e.key === "ArrowRight") RIGHT = false;
+    if(e.key === "ArrowLeft") LEFT = false;
+    if(e.key === "ArrowUp") UP = false;
+    if(e.key === "ArrowDown") DOWN = false;
 });
 
-function move(){
-    if(LEFT){
-        // Ball1.x--;
-        Ball2.x--; //control 2nd ball
-    }
-    if(RIGHT){
-        // Ball1.x++;
-        Ball2.x++; //control 2nd ball
-    }
-    if(UP){
-        // Ball1.y--;
-        Ball2.y--; //control 2nd ball
-    }
-    if(DOWN){
-        // Ball1.y++;
-        Ball2.y++; //control 2nd ball
-    }
+// Movement
+function move(ball){
+    if(LEFT) ball.x--;
+    if(RIGHT) ball.x++;
+    if(UP) ball.y--;
+    if(DOWN) ball.y++;
 }
 
 function mainLoop(){
-    ctx.clearRect(0,0,canvas.clientWidth, canvas.clientHeight);
-    move();
-    // drawBall(x,y,r);
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+
+    move(Ball1);
+
     Ball1.drawBall();
     Ball2.drawBall();
+
     requestAnimationFrame(mainLoop);
 }
 
 let Ball1 = new Ball(200,200,30);
-
-// lets create another ball
-
 let Ball2 = new Ball(100,100,20);
+
 requestAnimationFrame(mainLoop);
